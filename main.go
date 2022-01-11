4957f5d467e28912def9a41fcc5a372a247bc407
qiuqiu1999 <244907263@qq.com>
2022-01-11 10:36:19 +0800

    add global.setting

--- main.go
+++ main.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"go-blog/global"
 	"go-blog/internal/routers"
+	"go-blog/pkg/setting"
+	"log"
 	"net/http"
 	"time"
 )
 
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 
 	server := &http.Server{
-		Addr:           ":9090",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
 	server.ListenAndServe()
 }
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
