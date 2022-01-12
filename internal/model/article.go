395639b98349487e6e6a846040bc01f879227b85
qiuqiu1999 <244907263@qq.com>
2022-01-12 09:58:33 +0800

    ...

--- internal/model/article.go
+++ internal/model/article.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type Article struct {
 	*Model
 	Title         string `json:"title"`
@@ -9,6 +11,18 @@ type Article struct {
 	State         uint8  `json:"state"`
 }
 
-func (a Article) TableName() string {
+func (a *Article) TableName() string {
 	return "blog_tag"
+}
+
+func (a *Article) Create(db *gorm.DB) error {
+	return db.Create(a).Error
+}
+
+func (a *Article) Update(db *gorm.DB) error {
+	return db.Model(&Article{}).Where("id = ? AND is_del = ?", a.ID, 0).Update(a).Error
+}
+
+func (a *Article) Delete(db *gorm.DB) error {
+	return db.Where("id = ? AND is_del = ?", a.Model.ID, 0).Delete(a).Error
 }
--- internal/model/tag.go
+++ internal/model/tag.go
@@ -8,24 +8,24 @@ type Tag struct {
 	State uint8  `json:"state"`
 }
 
-func (t Tag) TableName() string {
+func (t *Tag) TableName() string {
 	return "blog_tag"
 }
 
-func (t Tag) Count(db *gorm.DB) (int, error) {
+func (t *Tag) Count(db *gorm.DB) (int, error) {
 	var count int
 	if t.Name != "" {
 		db = db.Where("name = ?", t.Name)
 	}
 	db = db.Where("state = ?", t.State)
-	if err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+	if err := db.Model(t).Where("is_del = ?", 0).Count(&count).Error; err != nil {
 		return 0, err
 	}
 
 	return count, nil
 }
 
-func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+func (t *Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
 	var tags []*Tag
 	var err error
 	if pageOffset >= 0 && pageSize > 0 {
@@ -42,14 +42,14 @@ func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
 	return tags, nil
 }
 
-func (t Tag) Create(db *gorm.DB) error {
-	return db.Create(&t).Error
+func (t *Tag) Create(db *gorm.DB) error {
+	return db.Create(t).Error
 }
 
-func (t Tag) Update(db *gorm.DB) error {
+func (t *Tag) Update(db *gorm.DB) error {
 	return db.Model(&Tag{}).Where("id = ? AND is_del = ?", t.ID, 0).Update(t).Error
 }
 
-func (t Tag) Delete(db *gorm.DB) error {
-	return db.Where("id = ? AND is_del = ?", t.Model.ID, 0).Delete(&t).Error
+func (t *Tag) Delete(db *gorm.DB) error {
+	return db.Where("id = ? AND is_del = ?", t.Model.ID, 0).Delete(t).Error
 }
