1fff63013c36fcaf930b216cf8ba751f1ef6163a
qiuqiu1999 <244907263@qq.com>
2022-01-14 15:31:12 +0800

    标签列表

--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -3,22 +3,21 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"go-blog/global"
+	"go-blog/internal/service"
 	"go-blog/pkg/app"
 	"go-blog/pkg/errcode"
 )
 
-type Tag struct {}
+type Tag struct{}
 
 func NewTag() Tag {
 	return Tag{}
 }
 
 func (t Tag) Get(c *gin.Context) {}
+
 func (t Tag) List(c *gin.Context) {
-	param := struct {
-		Name  string `form:"name" binding:"max=100"`
-		State uint8  `form:"state,default=1" binding:"oneof=0 1"`
-	}{}
+	param := service.TagListRequest{}
 	response := app.NewResponse(c)
 	valid, errs := app.BindAndValid(c, &param)
 	if !valid {
@@ -27,11 +26,28 @@ func (t Tag) List(c *gin.Context) {
 		return
 	}
 
-	response.ToResponse(gin.H{})
+	svc := service.New(c.Request.Context())
+	pager := app.Pager{Page: app.GetPage(c), PageSize: app.GetPageSize(c)}
+	totalRows, err := svc.CountTag(&service.CountTagRequest{Name: param.Name, State: param.State})
+	if err != nil {
+		global.Logger.Errorf("svc.CountTag err: %v", err)
+		response.ToErrorResponse(errcode.ErrorCountTagFail)
+		return
+	}
+
+	tags, err := svc.GetTagList(&param, &pager)
+	if err != nil {
+		global.Logger.Errorf("svc.GetTagList err: %v", err)
+		response.ToErrorResponse(errcode.ErrorGetTagListFail)
+		return
+	}
+
+	response.ToResponseList(tags, totalRows)
 	return
 }
+
 func (t Tag) Create(c *gin.Context) {}
+
 func (t Tag) Update(c *gin.Context) {}
+
 func (t Tag) Delete(c *gin.Context) {}
-
-
